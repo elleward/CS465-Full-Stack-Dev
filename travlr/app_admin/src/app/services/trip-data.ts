6d1145487ba878fb396ef1156650e40fde3eb426
Elle Ward <dariel.ward@snhu.edu>
2026-06-21 21:22:16 -0500

    Module 6 completed baseline

--- travlr/app_admin/src/app/services/trip-data.ts
+++ travlr/app_admin/src/app/services/trip-data.ts
@@ -21,4 +21,16 @@ export class TripData {
   addTrip(formData: Trip): Observable<Trip> {
     return this.http.post<Trip>(this.url, formData);
   }
+
+  getTrip(tripCode: string): Observable<Trip> {
+    // console.log('Inside TripData::getTrips')
+    return this.http.get<Trip>(this.url + '/' + tripCode);
+  }
+
+  updateTrip(formData: Trip): Observable<Trip> {
+    // console.log('Inside TripData::addTrips');
+    return this.http.put<Trip>(this.url + '/' + formData.code, formData);
+  }
+
 }
+    
--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -1,18 +1,15 @@
-const express = require('express'); // Express app
-const router = express.Router(); // Router logic
-
-// This is where we import the controllers we will use
+const express = require('express');
+const router = express.Router();
 const tripsController = require('../controllers/trips');
 
-// define route for our trips endpoint
 router
     .route('/trips')
-    .get(tripsController.tripsList) // GET Method routes tripList
-    .post(tripsController.tripsAddTrip); // POST Method Add a trip
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 
-// GET Method routes tripsFindByCode - requires parameter
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
