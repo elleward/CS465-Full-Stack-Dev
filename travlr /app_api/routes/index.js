8341263ee374a1ed6b6b0dbf5e7aa2549b281274
Elle Ward <dariel.ward@snhu.edu>
2026-06-25 19:36:50 -0500

    Module 7 WIP

---
+++ travlr /app_api/routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express'); // Express app
+const router = express.Router(); // Router logic
+
+// This is where we import the controllers we will use
+const tripsController = require('../controllers/trips');
+
+// define route for our trips endpoint
+router
+    .route('/trips')
+    .get(tripsController.tripsList) // GET Method routes tripList
+    .post(tripsController.tripsAddTrip); // POST Method Add a trip
+
+    // GET Method routes tripsFindByCode - requires parameter
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode)
+    .post(tripsController.tripsUpdateTrips);
+
+module.exports = router;
+
---
+++ travlr 3/app_api/controllers/trips.js
@@ -0,0 +1,112 @@
+const mongoose = require('mongoose');
+const Trip = require('../models/travlr'); // Register model
+const Model = mongoose.model('trips');
+
+// GET: /trips - list all the trips
+// Regardless of the putcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsList = async (req, res) => {
+    const q = await Model
+        .find({}) // No filter, return all records
+        .exec();
+
+    if (!q) { // Database returned no data
+        return res
+            .status(404)
+            .json({ message: "No trips found" });
+    } else { // Return resulting trip list
+        return res
+            .status(200)
+            .json(q);
+    }
+};
+
+
+// GET /trips/:tripCode - lists a single trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsFindByCode = async (req, res) => {
+    const q = await Model
+        .findOne({ 'code': req.params.tripCode })
+        .exec();
+
+    if (!q) {
+        // Database returned no data
+        return res
+            .status(404)
+            .json({ message: "No trip found" });
+    } else { // Return resulting trip list
+        return res
+            .status(200)
+            .json(q);
+    }
+};
+
+
+// POST: /trips - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async (req, res) => {
+    try {
+        const q = await Model.create({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        });
+
+        return res
+            .status(201)
+            .json(q);
+    } catch (err) {
+        return res
+            .status(400)
+            .json({ message: "Error adding trip", error: err.message });
+    }
+};
+
+// PUT: /trips/:tripCode - Updates an existing Trip
+const tripsUpdateTrip = async (req, res) => {
+    try {
+        const q = await Model
+            .findOneAndUpdate(
+                { 'code': req.params.tripCode },
+                {
+                    code: req.body.code,
+                    name: req.body.name,
+                    length: req.body.length,
+                    start: req.body.start,
+                    resort: req.body.resort,
+                    perPerson: req.body.perPerson,
+                    image: req.body.image,
+                    description: req.body.description
+                }
+            )
+            .exec();
+
+        if (!q) {
+            return res
+                .status(404)
+                .json({ message: 'Trip not found' });
+        } else {
+            return res
+                .status(200)
+                .json(q);
+        }
+    } catch (err) {
+        return res
+            .status(400)
+            .json({ message: 'Error updating trip', error: err.message });
+    }
+};
+
+module.exports = {
+    tripsList,
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
+};
